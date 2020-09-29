e17a8c75796ca886d4b00c08c126acd6b42ea213
Christopher Wells <Christopher.Wells@res-group.com>
2020-09-29 12:00:40 +0100

    Rewrite

--- game.py
+++ game.py
@@ -3,20 +3,25 @@ from score import Score, next_score
 
 class Game:
     def __init__(self, server_score: Score, receiver_score: Score):
-        self.server_score = server_score
-        self.receiver_score = receiver_score
+        self.server = TennisPlayer(server_score)
+        self.receiver = TennisPlayer(receiver_score)
 
-    def increment_player_score(self, player):
-        if player == "server":
-            if self.receiver_score != Score.ADVANTAGE:
-                self.server_score = next_score(self.server_score)
-            else:
-                self.receiver_score = Score.FORTY
-        elif player == "receiver":
-            if self.server_score != Score.ADVANTAGE:
-                self.receiver_score = next_score(self.receiver_score)
-            else:
-                self.server_score = Score.FORTY
+    def increment_server_score(self):
+        self._increment_player_score(self.server, self.receiver)
+
+    def increment_receiver_score(self):
+        self._increment_player_score(self.receiver, self.server)
+
+    def _increment_player_score(self, player, opponent):
+        if opponent.score != Score.ADVANTAGE:
+            player.score = next_score(player.score)
+        else:
+            opponent.score = Score.FORTY
 
     def format_score(self):
-        return f"{self.server_score}:{self.receiver_score}"
+        return f"{self.server.score}:{self.receiver.score}"
+
+
+class TennisPlayer:
+    def __init__(self, score: Score):
+        self.score = score
--- test_kata.py
+++ test_kata.py
@@ -9,40 +9,40 @@ def test_score_format():
 
 def test_server_score():
     game = Game(Score.ZERO, Score.ZERO)
-    game.increment_player_score("server")
+    game.increment_server_score()
     assert game.format_score() == '15:0'
 
 
 def test_receiver_score():
     game = Game(Score.ZERO, Score.ZERO)
-    game.increment_player_score("receiver")
+    game.increment_receiver_score()
 
     assert game.format_score() == '0:15'
 
 
 def test_score_fifteen():
     game = Game(Score.FIFTEEN, Score.FIFTEEN)
-    game.increment_player_score("receiver")
+    game.increment_receiver_score()
 
     assert game.format_score() == '15:30'
 
 
 def test_score_thirty():
     game = Game(Score.THIRTY, Score.THIRTY)
-    game.increment_player_score("server")
+    game.increment_server_score()
 
     assert game.format_score() == '40:30'
 
 
 def test_score_server_gets_advantage():
     game = Game(Score.FORTY, Score.FORTY)
-    game.increment_player_score("server")
+    game.increment_server_score()
 
     assert game.format_score() == 'A:40'
 
 
 def test_score_server_loses_advantage():
     game = Game(Score.ADVANTAGE, Score.FORTY)
-    game.increment_player_score("receiver")
+    game.increment_receiver_score()
 
     assert game.format_score() == '40:40'
