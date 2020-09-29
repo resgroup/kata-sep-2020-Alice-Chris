4cd719dac3a801753493c68d79e6e88c49e2ffc7
Christopher Wells <Christopher.Wells@res-group.com>
2020-09-29 10:39:13 +0100

    Add ability to increment up to 40

--- kata.py
+++ kata.py
@@ -1,7 +1,34 @@
-def a_function():
+from enum import Enum
 
-    return 1
 
-class Scorer:
+class Score(Enum):
+    ZERO = "0"
+    FIFTEEN = "15"
+    THIRTY = "30"
+    FORTY = "40"
+    ADVANTAGE = "A"
 
-    pass
+
+def next_score(current_score):
+    if current_score == Score.ZERO:
+        return Score.FIFTEEN
+    if current_score == Score.FIFTEEN:
+        return Score.THIRTY
+    if current_score == Score.THIRTY:
+        return Score.FORTY
+
+
+class Game:
+    def __init__(self, server_score: Score, receiver_score: Score):
+        self.server_score = server_score
+        self.receiver_score = receiver_score
+
+    def increment_server_score(self):
+        self.server_score = next_score(self.server_score)
+
+    def increment_receiver_score(self):
+        self.receiver_score = next_score(self.receiver_score)
+
+    def format_score(self):
+        return f"{self.server_score.value}:{self.receiver_score.value}"
+
--- test_kata.py
+++ test_kata.py
@@ -1,16 +1,20 @@
-import kata
+from kata import Score, Game
+
+
+def test_score_format():
+    game = Game(Score.ZERO, Score.ZERO)
+    assert game.format_score() == '0:0'
+
 
 def test_server_score():
+    game = Game(Score.ZERO, Score.ZERO)
+    game.increment_server_score()
 
-    score = kata.Scorer(0, 0)
-    score.server_scorer()
+    assert game.format_score() == '15:0'
 
-    assert score.server_scorer() == '15:0'
 
 def test_receiver_score():
+    game = Game(Score.ZERO, Score.ZERO)
+    game.increment_receiver_score()
 
-    score = kata.Scorer(0, 0)
-    score.receiver_scorer()
-
-    assert score.receiver_scorer() == '0:15'
-
+    assert game.format_score() == '0:15'
