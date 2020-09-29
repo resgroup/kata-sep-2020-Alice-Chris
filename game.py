539e819a602633e29cb9896cbaa08943675f5254
Christopher Wells <Christopher.Wells@res-group.com>
2020-09-29 11:49:17 +0100

    Making test pass

--- game.py
+++ game.py
@@ -8,9 +8,15 @@ class Game:
 
     def increment_player_score(self, player):
         if player == "server":
-            self.server_score = next_score(self.server_score)
+            if self.receiver_score != Score.ADVANTAGE:
+                self.server_score = next_score(self.server_score)
+            else:
+                self.receiver_score = Score.FORTY
         elif player == "receiver":
-            self.receiver_score = next_score(self.receiver_score)
+            if self.server_score != Score.ADVANTAGE:
+                self.receiver_score = next_score(self.receiver_score)
+            else:
+                self.server_score = Score.FORTY
 
     def format_score(self):
         return f"{self.server_score}:{self.receiver_score}"
