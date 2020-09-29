a16a4b45d9cf0c660d59ab0b63cc7783a9be478e
Christopher Wells <Christopher.Wells@res-group.com>
2020-09-29 10:41:41 +0100

    Add more tests

--- test_kata.py
+++ test_kata.py
@@ -18,3 +18,17 @@ def test_receiver_score():
     game.increment_receiver_score()
 
     assert game.format_score() == '0:15'
+
+
+def test_score_fifteen():
+    game = Game(Score.FIFTEEN, Score.FIFTEEN)
+    game.increment_receiver_score()
+
+    assert game.format_score() == '15:30'
+
+
+def test_score_thirty():
+    game = Game(Score.THIRTY, Score.THIRTY)
+    game.increment_server_score()
+
+    assert game.format_score() == '40:30'
